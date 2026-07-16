f2707a0ae16093a7f8bf8d791feb9379aaeb53fa
arahulram23 <rahulram23@hotmail.com>
2026-07-16 22:56:52 +0530

    Modules

---
+++ day09/main.py
@@ -0,0 +1,8 @@
+import calculator as cal
+import utils.math_utils as math_utils
+
+result = cal.add(5, 3)
+print(result)
+
+avg = math_utils.average([1, 2, 3, 4, 5])
+print(avg)
---
+++ day09/utils/math_utils.py
@@ -0,0 +1,8 @@
+def average(number: list[float])-> float:
+    if not number:
+        return 0.0
+    return sum(number) / len(number)
+
+    
+
+
